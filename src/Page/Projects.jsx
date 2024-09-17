b7f9b43ee95b396fa37ca89466a896ecabc1d5cc
judithmf00 <judithmf00@gmail.com>
2024-09-17 11:36:02 +0200

    Acabada la parte de los proyectos

--- src/Page/Projects.jsx
+++ src/Page/Projects.jsx
@@ -6,51 +6,58 @@ export default function Projects() {
       <h3 className="text-2xl font-bold mb-4 titlePage">Proyectos</h3>
       <section className='containerProjects'>
         <article className='project'>
-          <h4 className='text-xl font-semibold mb-2'>Contador de calorías</h4>
+          <h4 className='text-xl font-semibold mb-2'>Contador de calorías <span className='italic font-mono text-sm text-red-500'>(En desarrollo)</span></h4>
           <div className='flex containerProject'>
+            <a href="https://judithmf00.github.io/contadorCalorias/" target="_blank">
               <figure>
-                <img src="/img/contadorCalorias.PNG" alt="Página contador de calorías" />
-                <i className="fas fa-eye"></i>
-              </figure>
-              <div>
-                <p className='project__description'>
-                  Este proyecto permite registrar las calorías que quemamos o consumimos en función de diferentes actividades 
-                  (por ejemplo, <span className='italic'>bicicleta</span>, <span className='italic'>caminar</span>) y comidas 
-                  (como <span className='italic'>manzana</span>, <span className='italic'>hamburguesa</span>).
-                  La comida <span className='font-semibold italic'>incrementa</span> las calorías, mientras que el ejercicio <span className='font-semibold italic'>las reduce</span>, 
-                  mostrando al final la diferencia para saber si <span className='font-bold'>quemamos</span> más de lo que consumimos.
-                </p>
-                <aside>
-                  <p className='font-semibold'>Tecnologías empleadas:</p>
-                  <div>
-                    <i className="fab fa-html5" style={{ color: '#E34F26' }}></i>
-                    <i className="fab fa-css3-alt" style={{ color: '#1572B6' }}></i>
-                    <i className="fab fa-js" style={{ color: '#F7E018' }}></i>
-                    <i className="fab fa-react" style={{ color: '#61DAFB' }}></i>
-                    <i className="fab fa-sass" style={{ color: '#CC6699' }}></i>
-                    <img src="/img/tailwincss.png" alt="Icono tailwincss" />
-                    <img src="/img/typescript.png" alt="Icono typescript" />
-                  </div>
-                </aside>
-              </div>
+                  <img src="/img/contadorCalorias.PNG" alt="Página contador de calorías" />
+                  <i className="fas fa-eye"></i>
+                </figure>
+            </a>
+              
+            <div>
+              <p className='project__description'>
+                Este proyecto permite registrar las calorías que quemamos o consumimos en función de diferentes actividades 
+                (por ejemplo, <span className='italic'>bicicleta</span>, <span className='italic'>caminar</span>) y comidas 
+                (como <span className='italic'>manzana</span>, <span className='italic'>hamburguesa</span>).
+                La comida <span className='font-semibold italic'>incrementa</span> las calorías, mientras que el ejercicio <span className='font-semibold italic'>las reduce</span>, 
+                mostrando al final la diferencia para saber si <span className='font-bold'>quemamos</span> más de lo que consumimos.
+              </p>
+              <aside>
+                <p className='font-semibold'>Tecnologías empleadas:</p>
+                <div>
+                  <i className="fab fa-html5" style={{ color: '#E34F26' }}></i>
+                  <i className="fab fa-css3-alt" style={{ color: '#1572B6' }}></i>
+                  <i className="fab fa-js" style={{ color: '#F7E018' }}></i>
+                  <i className="fab fa-react" style={{ color: '#61DAFB' }}></i>
+                  <i className="fab fa-sass" style={{ color: '#CC6699' }}></i>
+                  <img src="/img/tailwincss.png" alt="Icono tailwincss" />
+                  <img src="/img/typescript.png" alt="Icono typescript" />
+                </div>
+              </aside>
+            </div>
           </div>
           <div className="buttonsProjects">
-            <a href="https://github.com/judithmf00/contadorCalorias"><i className="fab fa-github" style={{ color: '#fefefe' }}></i></a>
-            <a href=""><i className="fab fa-figma" style={{ color: '#fefefe' }}></i></a>
+            <a href="https://github.com/judithmf00/contadorCalorias" target="_blank"><i className="fab fa-github" style={{ color: '#fefefe' }}></i></a>
+            <a href="https://www.figma.com/proto/xHFD15IQqfLJLGCjN4ls7e/Contador-de-calorias?node-id=1-2&node-type=canvas&t=lpBryj9nEVgNZUra-1&scaling=contain&content-scaling=fixed&page-id=0%3A1" target="_blank"><i className="fab fa-figma" style={{ color: '#fefefe' }}></i></a>
           </div>
           
         </article>
         <article className='project'>
           <h4 className='text-xl font-semibold mb-2'>Calculadora de propinas</h4>
           <div className='flex containerProject'>
-              <figure>
-                <img src="/img/calculadoraPropinas.PNG" alt="Página calculadora de propinas" />
-              </figure>
+              <a href="https://judithmf00.github.io/contadorCalorias/" target="_blank">
+                <figure>
+                  <img src="/img/calculadoraPropinas.PNG" alt="Página calculadora de propinas" />
+                  <i className="fas fa-eye"></i>
+                </figure>
+              </a>
+              
               <div>
-              <p className='project__description'>
-                Incluye un <span className='italic'>menú</span> con platos de un restaurante que se pueden añadir a la comanda. 
-                Además, permite seleccionar el <span className='italic font-semibold'>porcentaje de propina</span> a dejar y realiza el cálculo del <span className='italic'>subtotal</span>, la <span className='italic'>propina</span> y el <span className='italic'>total</span>.
-              </p>
+                <p className='project__description'>
+                  Incluye un <span className='italic'>menú</span> con platos de un restaurante que se pueden añadir a la comanda. 
+                  Además, permite seleccionar el <span className='italic font-semibold'>porcentaje de propina</span> a dejar y realiza el cálculo del <span className='italic'>subtotal</span>, la <span className='italic'>propina</span> y el <span className='italic'>total</span>.
+                </p>
                 <aside>
                   <p className='font-semibold'>Tecnologías empleadas:</p>
                   <div>
@@ -66,39 +73,47 @@ export default function Projects() {
               </div>
           </div>
           <div className="buttonsProjects">
-            <a href="https://github.com/judithmf00/calculadoraComandas"><i className="fab fa-github" style={{ color: '#fefefe' }}></i></a>
-            <a href=""><i className="fab fa-figma" style={{ color: '#fefefe' }}></i></a>
+            <a href="https://github.com/judithmf00/calculadoraComandas" target="_blank"><i className="fab fa-github" style={{ color: '#fefefe' }}></i></a>
+            <a href="https://www.figma.com/proto/pCDgqQQl6wuH9pM372ujxc/Calculadora-comandas?t=GLPD6Q2KJzBRKzHa-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&node-id=1-2" target="_blank"><i className="fab fa-figma" style={{ color: '#fefefe' }}></i></a>
           </div>
         </article>
         <article className='project'>
           <h4 className='text-xl font-semibold mb-2'>Películas del studio ghibli</h4>
           <div className='flex containerProject'>
+            <a href="https://judiith-mf.github.io/PeliculasStudioGhibli/" target="_blank">
               <figure>
                 <img src="/img/studioGhibli.PNG" alt="Página studio ghibli" />
+                <i className="fas fa-eye"></i>
               </figure>
-              <div>
-                <p className='project__description'>
-                  <span className='font-semibold'>Primer proyecto realizado en React,</span> los datos de las <span className='font-semibold'>películas</span> se obtienen de una <span className='font-semibold'>API</span>, donde se listan todas las películas del <span className='font-semibold'>Studio Ghibli</span>. 
-                  Al seleccionar una, se puede ver su <span className='italic'>detalle</span>, incluyendo información como la <span className='italic'>puntuación</span> y los <span className='italic'>personajes</span>.
-                </p>
-                <aside>
-                  <p className='font-semibold'>Tecnologías empleadas:</p>
-                  <div>
-                    <i className="fab fa-html5" style={{ color: '#E34F26' }}></i>
-                    <i className="fab fa-css3-alt" style={{ color: '#1572B6' }}></i>
-                    <i className="fab fa-js" style={{ color: '#F7E018' }}></i>
-                    <i className="fab fa-react" style={{ color: '#61DAFB' }}></i>
-                  </div>
-                </aside>
-              </div>
+            </a>
+              
+            <div>
+              <p className='project__description'>
+                <span className='font-semibold'>Primer proyecto realizado en React,</span> los datos de las <span className='font-semibold'>películas</span> se obtienen de una <span className='font-semibold'>API</span>, donde se listan todas las películas del <span className='font-semibold'>Studio Ghibli</span>. 
+                Al seleccionar una, se puede ver su <span className='italic'>detalle</span>, incluyendo información como la <span className='italic'>puntuación</span> y los <span className='italic'>personajes</span>.
+              </p>
+              <aside>
+                <p className='font-semibold'>Tecnologías empleadas:</p>
+                <div>
+                  <i className="fab fa-html5" style={{ color: '#E34F26' }}></i>
+                  <i className="fab fa-css3-alt" style={{ color: '#1572B6' }}></i>
+                  <i className="fab fa-js" style={{ color: '#F7E018' }}></i>
+                  <i className="fab fa-react" style={{ color: '#61DAFB' }}></i>
+                </div>
+              </aside>
+            </div>
           </div>
         </article>
         <article className='project'>
           <h4 className='text-xl font-semibold mb-2'>Coctelería</h4>
           <div className='flex containerProject'>
-              <figure>
-                <img src="/img/cocteles.PNG" alt="Página de coctelería" />
-              </figure>
+              <a href="https://judithmf00.github.io/coctelesJavascriptApi/" target='_black'>
+                <figure>
+                  <img src="/img/cocteles.PNG" alt="Página de coctelería" />
+                  <i className="fas fa-eye"></i>
+                </figure>
+              </a>
+              
               <div>
                 <p className='project__description'>
                   <span className='font-semibold'>Proyecto de clase </span> en el cual debíamos crear una aplicación utilizando una <span className='font-semibold'>API</span>. En mi caso, utilicé <span className='italic font-semibold'>'thecocktaildb'</span>. 
@@ -118,36 +133,39 @@ export default function Projects() {
               </div>
           </div>
           <div className="buttonsProjects">
-            <a href="https://github.com/judithmf00/coctelesJavascriptApi"><i className="fab fa-github" style={{ color: '#fefefe' }}></i></a>
+            <a href="https://github.com/judithmf00/coctelesJavascriptApi" target='_black'><i className="fab fa-github" style={{ color: '#fefefe' }}></i></a>
           </div>
         </article>
         <article className='project'>
           <h4 className='text-xl font-semibold mb-2'>Tienda de guitarras</h4>
           <div className='flex containerProject'>
+            <a href="">
               <figure>
                 <img src="/img/shopGuitar.PNG" alt="Página tienda de guitarras" />
+                <i className="fas fa-eye"></i>
               </figure>
-              <div>
-                <p className='project__description'>
-                  <span className='italic'>Catálogo</span> donde puedes <span className='italic font-semibold'>agregar guitarras</span> al carrito, ver un <span className='italic'>resumen del pedido</span>, 
-                  <span className='italic'>modificar la cantidad</span> de productos o <span className='italic'>eliminar artículos</span>, y visualizar el <span className='italic font-semibold'>total a pagar</span>.
-                </p>
+            </a>
+            <div>
+              <p className='project__description'>
+                <span className='italic'>Catálogo</span> donde puedes <span className='italic font-semibold'>agregar guitarras</span> al carrito, ver un <span className='italic'>resumen del pedido</span>, 
+                <span className='italic'>modificar la cantidad</span> de productos o <span className='italic'>eliminar artículos</span>, y visualizar el <span className='italic font-semibold'>total a pagar</span>.
+              </p>
 
-                <aside>
-                  <p className='font-semibold'>Tecnologías empleadas:</p>
-                  <div>
-                    <i className="fab fa-html5" style={{ color: '#E34F26' }}></i>
-                    <i className="fab fa-css3-alt" style={{ color: '#1572B6' }}></i>
-                    <i className="fab fa-js" style={{ color: '#F7E018' }}></i>
-                    <i className="fab fa-react" style={{ color: '#61DAFB' }}></i>
-                    <i className="fab fa-sass" style={{ color: '#CC6699' }}></i>
-                  </div>
-                </aside>
-              </div>
+              <aside>
+                <p className='font-semibold'>Tecnologías empleadas:</p>
+                <div>
+                  <i className="fab fa-html5" style={{ color: '#E34F26' }}></i>
+                  <i className="fab fa-css3-alt" style={{ color: '#1572B6' }}></i>
+                  <i className="fab fa-js" style={{ color: '#F7E018' }}></i>
+                  <i className="fab fa-react" style={{ color: '#61DAFB' }}></i>
+                  <i className="fab fa-sass" style={{ color: '#CC6699' }}></i>
+                </div>
+              </aside>
+            </div>
           </div>
           <div className="buttonsProjects">
-            <a href="https://github.com/judithmf00/shopGuitar"><i className="fab fa-github" style={{ color: '#fefefe' }}></i></a>
-            <a href=""><i className="fab fa-figma" style={{ color: '#fefefe' }}></i></a>
+            <a href="https://github.com/judithmf00/shopGuitar" target='_black'><i className="fab fa-github" style={{ color: '#fefefe' }}></i></a>
+            <a href="https://www.figma.com/proto/h4SC1UHcyWvel2gHOoX7sg/Shop-Guitar?node-id=1-2&t=4g9z4EyVwLwSCzUG-1" target='_black'><i className="fab fa-figma" style={{ color: '#fefefe' }}></i></a>
           </div>
         </article>
       </section>
